efbb4364ad6cff83a17cfe725f90f42fa9beb3a3
Harsh Patel <harsh.patel@iquadra.com>
2025-07-01 14:26:03 -0400

    fix: update how the server listens on the port

--- server/index.ts
+++ server/index.ts
@@ -62,14 +62,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
-  server.listen(
-    {
-      port,
-      host: "localhost",
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    }
-  );
+  server.listen(port, "localhost", () => {
+    log(`serving on port ${port}`);
+  });
 })();
